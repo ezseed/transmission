5ab034eb98af36e2bb2ba2e732f2f694ffcb79d6
soyuka <soyuka@gmail.com>
2014-06-22 15:10:59 +0200

    Added settings

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var p = require('path')
 
 module.exports = function(shell) {
-  if(['install', 'useradd', 'userdel', 'daemon'].indexOf(shell) !== -1)
+  if(shell == 'settings') {
+    return p.join(__dirname, 'settings.json')
+  } else if(['install', 'useradd', 'userdel', 'daemon'].indexOf(shell) !== -1)
     return p.join(__dirname, shell) + '.sh'
   else
     throw new Error('Shell %s does not exists', shell)
