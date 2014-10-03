26ed864bc3ac4117c7fb465e43f8e853cc026f61
soyuka <soyuka@gmail.com>
2014-10-03 08:04:14 +0200

    feat(): Ads passwd

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var p = require('path')
 module.exports = function(shell) {
   if(shell == 'settings') {
     return p.join(__dirname, 'settings.json')
-  } else if(['install', 'useradd', 'userdel', 'daemon'].indexOf(shell) !== -1)
+  } else if(['install', 'useradd', 'userdel', 'daemon'].indexOf(shell) !== -1) {
     return p.join(__dirname, shell) + '.sh'
+  } else if(shell == 'passwd') {
+    return p.join(__dirname, 'passwd.py')
+  }
   else
     throw new Error('Shell %s does not exists', shell)
 }
