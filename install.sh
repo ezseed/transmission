cb78f7741d341f231d8deaef6f5faa34d858bcae
soyuka <soyuka@gmail.com>
2014-10-03 19:36:02 +0200

    fix(install): exit instead of return

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [ -f /etc/init.d/transmission-daemon ]
 then
     echo "Transmission is already installed"
-    return
+    exit 0
 else
 	apt-get install transmission-daemon -y
 	echo "Stopping transmission-daemon"
